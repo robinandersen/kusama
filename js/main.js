586ff974a8eb541594155f9863c4a761be962e85
Robin Andersen <post@robinandersen.no>
2015-11-04 12:31:39 +0100

    Changes a number

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var mouseX;
 
 $(document).mousemove(function(e) {
-   mouseX = e.pageX * -0.2; 
+   mouseX = e.pageX * -0.3; 
 
 	$("html").mouseover(function(){
 	  $('#heroimage').css({'background-position':mouseX});
